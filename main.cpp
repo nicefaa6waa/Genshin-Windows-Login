4e98feb8ee2e2c4eb6566ea614c5c1fef5118ac0
churchy <129511714+nicefaa6waa@users.noreply.github.com>
2024-01-14 17:07:02 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+    // Set QML_XHR_ALLOW_FILE_READ to 1
+    qputenv("QML_XHR_ALLOW_FILE_READ", QByteArray("1"));
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreated,
+        &app,
+        [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
